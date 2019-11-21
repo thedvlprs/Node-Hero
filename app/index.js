6975cfd9b7fbc4d69f1e8a9013a7daa6adc3a06b
Fyunka <fyunkafinka@hotmail.com>
2019-11-21 22:58:58 +0500

    init: start project. chapter-1: getting started with nodejs

---
+++ app/index.js
@@ -0,0 +1,5 @@
+const calc = require('./calc');
+const numbersToAdd = [3, 4, 10, 2];
+
+const result = calc.sum(numbersToAdd);
+console.log(`The result is: ${result}`);
---
+++ index.js
@@ -0,0 +1,2 @@
+// console.log('hello from Node.js')
+require('./app/index');
