49c2926ffca19b27175ce6eb2fe05157be554cac
Fyunka <fyunkafinka@hotmail.com>
2019-11-22 17:17:11 +0500

    chapter-3: async programming in node.js - event loop

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 // console.log('hello from Node.js')
 // require('./app/index');
 
-const _ = require('lodash');
+// const _ = require('lodash');
 
-_.assign({ a: 1 }, { b: 2 }, { c: 3 });
-// → { 'a': 1, 'b': 2, 'c': 3 }
+// _.assign({ a: 1 }, { b: 2 }, { c: 3 });
+
+const fs = require('fs');
+
+console.log('start reading a file...');
+
+fs.readFile('file.md', 'utf-8', function(err, content) {
+  if (err) {
+    console.log('error happened during reading the file');
+    return console.log(err);
+  }
+  console.log(content);
+});
+
+console.log('end of the file');
+
+function stats(file) {
+  return new Promise((resolve, reject) => {
+    fs.stat(file, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+Promise.all([stats('file1'), stats('file2'), stats('file3')])
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
