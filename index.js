ca6cad63dca88f0a5f37abe4115bff16b2a1f9a3
Fyunka <fyunkafinka@hotmail.com>
2019-11-22 16:20:15 +0500

    chapter-2: using npm

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // console.log('hello from Node.js')
-require('./app/index');
+// require('./app/index');
+
+const _ = require('lodash');
+
+_.assign({ a: 1 }, { b: 2 }, { c: 3 });
+// → { 'a': 1, 'b': 2, 'c': 3 }
